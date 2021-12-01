01ec689a2f67e543b758f354643a9b611306c126
riegersan <rieger_san@icloud.com>
2021-12-01 07:47:14 +0100

    Test with Day 01 of 2020 will be changed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun List<Int>.findPairOfSum(sum: Int): Pair<Int, Int>? {
+        val complements = associateBy { sum - it }
+        return firstNotNullOfOrNull { number ->
+            val complement = complements[number]
+            if (complement != null) Pair(number, complement) else null
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun List<Int>.findTripleOfSum() = firstNotNullOfOrNull { x ->
+        val pair = findPairOfSum(2020 - x)
+        if (pair != null) Triple(x, pair.first, pair.second) else null
+    }
+
+    fun part1(input: List<String>): Int? {
+        val numbers = input.map { it.toInt() }
+
+        val pair = numbers.findPairOfSum(2020)
+
+        return pair?.let { (x,y) -> x*y }
+    }
+
+    fun part2(input: List<String>): Int? {
+        val numbers = input.map { it.toInt() }
+
+        val triple = numbers.findTripleOfSum()
+
+        return triple?.let { (x,y,z) -> x*y*z }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 514579)
+    check(part2(testInput) == 241861950)
 
     val input = readInput("Day01")
     println(part1(input))
