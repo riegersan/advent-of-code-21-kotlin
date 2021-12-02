d1c61284701aa7155944b33c333b0018d1b0a77e
riegersan <rieger_san@icloud.com>
2021-12-02 06:48:14 +0100

    added Day02 Part01 and Part02

--- src/Utils.kt
+++ src/Utils.kt
@@ -5,7 +5,10 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src/${name.lowercase().removeSuffix("_test")}", "$name.txt").readLines()
+fun readInput(name: String) = File(
+    "src/${name.lowercase()
+        .removeSuffix("_test")}",
+    "$name.txt").readLines()
 
 /**
  * Converts string to md5 hash.
--- src/day02/day02.kt
+++ src/day02/day02.kt
@@ -4,22 +4,50 @@ import readInput
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        val numbers = input.map { it.toInt() }
+    fun List<String>.listToPair() = flatMap { it.split(" ") }
+        .chunked(2)
+        .map { Pair(it[0], it[1]) }
 
-        return 0
+    fun part1(input: List<String>): Int {
+        var position = 0
+        var depth = 0
+
+        input.listToPair()
+            .forEach {
+                when (it.first) {
+                    "forward" -> position += it.second.toInt()
+                    "down" -> depth += it.second.toInt()
+                    "up" -> depth -= it.second.toInt()
+                }
+            }
+
+        return position * depth
     }
 
     fun part2(input: List<String>): Int {
-        val numbers = input.map { it.toInt() }
+        var position = 0
+        var depth = 0
+        var aim = 0
 
-        return 0
+        input.listToPair()
+            .forEach {
+                when (it.first) {
+                    "forward" -> {
+                        position += it.second.toInt()
+                        depth += aim * it.second.toInt()
+                    }
+                    "down" -> aim += it.second.toInt()
+                    "up" -> aim -= it.second.toInt()
+                }
+            }
+
+        return position * depth
     }
 
 // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
 
     val input = readInput("Day02")
     println("Answer Part 1: ${part1(input)}")
