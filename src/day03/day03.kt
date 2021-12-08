3487db37f1ad363363b595a6ba8713a7ab97e19a
riegersan <rieger_san@icloud.com>
2021-12-08 17:53:41 +0100

    Day 03 Part 02

--- src/day03/day03.kt
+++ src/day03/day03.kt
@@ -22,15 +22,51 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
+        val oxygenRating = input.toMutableList()
+        val carbonDioxidRating = input.toMutableList()
 
+        var oxygen = ""
 
-        return 0
+        input[0].forEachIndexed{ index, _ ->
+            when {
+                oxygenRating.count { it[index] == '0' } > oxygenRating.count { it[index] == '1' } -> {
+                    oxygenRating.removeAll { it[index] == '1' }
+                }
+                else -> {
+                    oxygenRating.removeAll { it[index] == '0' }
+                }
+            }
+            if(oxygenRating.size == 1) oxygenRating[0].also { oxygen = it }
+        }
+        print("")
+
+        var co2 = ""
+        input[0].forEachIndexed{ index, _ ->
+            when {
+                carbonDioxidRating.count { it[index] == '0' } != carbonDioxidRating.count { it[index] == '1' } -> {
+                    when {
+                        carbonDioxidRating.count { it[index] == '0' } < carbonDioxidRating.count { it[index] == '1' } -> {
+                            carbonDioxidRating.removeAll { it[index] == '1' }
+                        }
+                        else -> {
+                            carbonDioxidRating.removeAll { it[index] == '0' }
+                        }
+                    }
+                }
+                else -> {
+                    carbonDioxidRating.removeAll { it[index] == '1' }
+                }
+            }
+            if(carbonDioxidRating.size == 1) carbonDioxidRating[0].also { co2 = it }
+        }
+
+        return Integer.parseInt(oxygen, 2) * Integer.parseInt(co2, 2)
     }
 
 // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 198)
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 230)
 
     val input = readInput("Day03")
     println("Answer Part 1: ${part1(input)}")
